72849ec7a433f8333c5f608268ef5ea97cf46ce9
lukas1gb <lukas.prestes@escola.pr.gov.br>
2026-04-01 12:28:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.get('/banana', (req, res) => {
 
 app.get('/pedido', (req, res) => {
 
-    let precoCafe = 8;
+    let precoCafe = 12;
     let total = 0;
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 7; i++) {
         total += precoCafe;
         console.log(`fazendo um cafe ${i + 1}. Total acumulado:R$ ${total}`);
         
@@ -22,9 +22,14 @@ app.get('/pedido', (req, res) => {
     }
 
 if (total >=30) {
-    total = total - 3
+    total = total - 5
     console.log ("desconto aplicado:"+ total)
 }
+else if (total > 50){
+    console.log("recebestes um croissant de brinde")
+}
+
+
 else {
     console.log ("Sem desconto")
 }
